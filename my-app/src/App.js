e827782d639db18a99b042af3aced8c39d075102
pray1n <89249367+pray1n@users.noreply.github.com>
2022-08-06 15:23:18 +0200

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,7 +12,7 @@ function App() {
 
     try {
     const response = await fetch(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_bADom2GJMtoutlcVw7Q9JoRzQWc5O&ipAddress`)
-    
+
 
     if (response.ok) {
       console.log("Reponse is Okay")
@@ -26,20 +26,21 @@ function App() {
     }
 
 
-
+const lat = () =>  !loadedIP ? "Data is Loading" : loadedIP.location.lat
+const lng = () =>  !loadedIP ? "Data is Loading" : loadedIP.location.lng
     return(
     <div>
     <Alert severity="success"> Working</Alert>
 
-    {/* <Map height={300} width={450} defaultCenter={[!loadedIP ? "Data is Loading" : loadedIP.location.lat, !loadedIP ? "Data is Loading" : loadedIP.location.lng]} defaultZoom={11}>
-      <Marker width={50} anchor={[!loadedIP ? "Data is Loading" : loadedIP.location.lat, !loadedIP ? "Data is Loading" : loadedIP.location.lng]} />
-    </Map>  */}
+    <Map height={300}  defaultCenter={[lat, lng]} defaultZoom={11}>
+      <Marker width={50} anchor={[lat, lng]} />
+    </Map> 
 
     {/* <Map height={300} width={450} defaultCenter={[loadedIP.location.lat, loadedIP.location.lng]} defaultZoom={11}>
       <Marker width={50} anchor={[loadedIP.location.lat, loadedIP.location.lng]} />
     </Map> */}
 
-    {/* <Map height={300} width={450} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
+     {/* <Map height={300} width={450} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
       <Marker width={50} anchor={[50.879, 4.6997]} />
     </Map> */}
 
@@ -49,7 +50,10 @@ function App() {
     <p>{!loadedIP ? "Data is Loading" : loadedIP.location.lat}</p>
     <p>{!loadedIP ? "Data is Loading" : loadedIP.location.lng}</p>
     <p>{!loadedIP ? "Data is Loading" : loadedIP.location.country}</p>
-    <img src="https://flagcdn.com/120x90/de.png"></img>
+    <img
+      src={`https://flagcdn.com/h120/${!loadedIP ? "Data is Loading" : loadedIP.location.country.toLowerCase()}.png`}
+      height="120"
+      alt="New Mexico"></img>
 
 
     </div>)
