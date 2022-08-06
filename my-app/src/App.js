11c3c4798abe25daa5040b02cc7c57c381b9b2b6
pray1n <89249367+pray1n@users.noreply.github.com>
2022-08-06 11:21:50 +0200

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import Alert from '@mui/material/Alert';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div> What'sMyIP</div>
-  );
+  const [loadedIP, setLoadedIP] = useState("")
+  useEffect(() => { getData()}, [])
+  const getData = async () => {
+     
+  
+    try {
+    const response = await fetch(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_bADom2GJMtoutlcVw7Q9JoRzQWc5O&ipAddress=`)
+    console.log(response)
+    
+    if (response.ok) {
+      console.log("Reponse is Okay")
+      const result = await response.json();
+      setLoadedIP(result)}
+      return(
+        <div>
+        <Alert severity="success">This is a success alert — check it out!</Alert>
+        </div>
+    )
+
+    } catch (e) {
+      console.log('Error')
+    }
+    }
+    
+
+    return(
+    <div>
+    <p>{!loadedIP ? "Data is Loading" : loadedIP.ip}</p>
+    <p>{!loadedIP ? "Data is Loading" : loadedIP.location.country}</p>
+    <p>{!loadedIP ? "Data is Loading" : loadedIP.location.region}</p>
+    <p>{!loadedIP ? "Data is Loading" : loadedIP.location.city}</p>
+    <p>{!loadedIP ? "Data is Loading" : loadedIP.location.postcode}</p>
+
+    
+    </div>)
 }
 
 export default App;
